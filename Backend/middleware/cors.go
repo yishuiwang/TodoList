a61fa68d6faf6186761adaa7ed65da665badc204
yishuiwang <yishuiwang163@gmail.com>
2022-07-21 11:34:37 +0800

    解决跨域问题

---
+++ Backend/middleware/cors.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// 跨域
+func Cors() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(http.StatusNoContent)
+		} else {
+			ctx.Next()
+		}
+	}
+}
--- Backend/routers/routers.go
+++ Backend/routers/routers.go
@@ -3,12 +3,14 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"go.mod/controller"
+	"go.mod/middleware"
 	"net/http"
 )
 
-func SetupRouter() *gin.Engine{
-	http.Handle("/static/",http.FileServer(http.Dir("static/")))
+func SetupRouter() *gin.Engine {
+	http.Handle("/static/", http.FileServer(http.Dir("static/")))
 	r := gin.Default()
+	r.Use(middleware.Cors())
 	// v1
 	v1Group := r.Group("v1")
 	{
